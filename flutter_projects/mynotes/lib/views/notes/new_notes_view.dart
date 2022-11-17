1ab611481aedb3ec05fcae3587ab54ad2cb3e2bf
Jay Yoo <jay.yoo@live.com>
2022-11-17 13:07:15 -0500

    added new methods to add new notes onto the new_notes_view

--- flutter_projects/mynotes/lib/views/notes/new_notes_view.dart
+++ flutter_projects/mynotes/lib/views/notes/new_notes_view.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:mynotes/services/auth/auth_service.dart';
+import 'package:mynotes/services/crud/notes_service.dart';
 
 class NewNotesView extends StatefulWidget {
   const NewNotesView({super.key});
@@ -10,6 +12,36 @@ class NewNotesView extends StatefulWidget {
 }
 
 class NewNotesViewState extends State<NewNotesView> {
+  DatabaseNotes? _note;
+  late final NotesService _notesService;
+  late final TextEditingController _textController;
+
+  Future<DatabaseNotes> createNewNote() async {
+    final existingNote = _note;
+    if (existingNote != null) {
+      return existingNote;
+    }
+    final currentUser = AuthService.firebase().currentUser!;
+    final email = currentUser.email!;
+    final owner = await _notesService.getUser(email: email);
+    return await _notesService.createNote(owner: owner);
+  }
+
+  void _deleteNoteIfTextIsEmpty() {
+    final note = _note;
+    if (_textController.text.isEmpty && note != null) {
+      _notesService.deleteNote(id: note.id);
+    }
+  }
+
+  void _saveNoteIfTextNotEmpty() async {
+    final note = _note;
+    final text = _textController.text;
+    if (_textController.text.isNotEmpty && note != null) {
+      await _notesService.updateNote(note: note, text: text);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
